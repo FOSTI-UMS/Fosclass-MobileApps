05e3dcee0f3252d49c19bf1b979e184f1684aa61
alfiankan <alfiankan19@gmail.com>
2021-03-19 14:05:06 +0700

    fosclass activity

--- app/src/main/java/com/example/fosclass1/HasilActivity.java
+++ app/src/main/java/com/example/fosclass1/HasilActivity.java
@@ -2,7 +2,9 @@ package com.example.fosclass1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HasilActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class HasilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
+        Intent intent = getIntent();
+        String bgn = intent.getStringExtra("BANGUNDATAR");
+
+        TextView hasil = findViewById(R.id.hasil);
+        hasil.setText(bgn);
+
     }
 }
--- app/src/main/java/com/example/fosclass1/MainActivity.java
+++ app/src/main/java/com/example/fosclass1/MainActivity.java
@@ -16,14 +16,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         CardView btnsegitiga = findViewById(R.id.tombolsegitiga);
+        CardView btnpersegi = findViewById(R.id.persegi);
+        CardView btnlingkaran = findViewById(R.id.lingkaran);
+        CardView btnpp = findViewById(R.id.persegipanjang);
 
         /* user input angka 1 -> user input sngka 2 -> (klik)button -> (angka1+angka2)*/
         btnsegitiga.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                Intent activityhasil = new Intent(MainActivity.this,HasilActivity.class);
+               activityhasil.putExtra("BANGUNDATAR","Segitiga");
                startActivity(activityhasil);
+            }
+        });
+
+
+
+
+        btnpersegi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityhasil = new Intent(MainActivity.this,HasilActivity.class);
+                startActivity(activityhasil);
+            }
+        });
+
+        btnlingkaran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityhasil = new Intent(MainActivity.this,HasilActivity.class);
+                startActivity(activityhasil);
+            }
+        });
+
+        btnpp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityhasil = new Intent(MainActivity.this,HasilActivity.class);
+                startActivity(activityhasil);
             }
         });
     }
